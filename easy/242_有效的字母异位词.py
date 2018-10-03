5c93cc13145a07df0d05301a523b1be489e05e00
DinY_xiaohuli <1127607894@qq.com>
2018-10-03 08:33:30 +0800

    181003

--- easy/242_有效的字母异位词.py
+++ easy/242_有效的字母异位词.py
@@ -9,28 +9,10 @@ class Solution:
         :type s: str
         :type t: str
         :rtype: bool
-        
-        dict_s = {};
-        for i in s:
-            if i in dict_s:
-                dict_s[i] = dict_s[i]+1;
-            else:
-                dict_s[i] = 1;
-        
-        dict_t = {};
-        for j in t:
-            if j in dict_t:
-                dict_t[j] = dict_t[j]+1;
-            else:
-                dict_t[j] = 1;
-        
-        if len(dict_s) != len(dict_t):
-            return False;
-                
-        print(str(dict_s),str(dict_t));
-        for key,val in dict_s.items():
-            if val != dict_t.get(key,0):
-                return False;
-        return True
         """
+        #解法1
         return set(s) == set(t) and all([s.count(i) == t.count(i) for i in string.ascii_letters ])
+
+        #解法2
+        from collections import Counter
+        return Counter(s) == Counter(t);

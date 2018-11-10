45b15f6b0b5442ced86acc807baa0169802beae8
DinY_xiaohuli <1127607894@qq.com>
2018-11-10 21:23:21 +0800

    181110

--- normal/46_全排列.py
+++ normal/46_全排列.py
@@ -12,3 +12,23 @@ class Solution:
             for j in self.permute(next_num):
                 res.append([num]+j);
         return res;
+
+#回溯法------------------------------------------------
+class Solution:
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = self.backtracking([],[],nums);
+        return res;
+        
+    def backtracking(self,sub,res,nums):
+        if len(nums) == 0:
+            res.append(sub[:]);
+        else:
+            for i in range(len(nums)):
+                sub.append(nums[i]);
+                self.backtracking(sub,res,nums[:i]+nums[i+1:]);
+                sub.pop();
+        return res;
--- normal/78_子集.py
+++ normal/78_子集.py
@@ -8,3 +8,22 @@ class Solution:
         for i in nums:
             res = res + [[i] + l for l in res ];
         return res;
+
+#子集-回溯 LTE--------------------------------------
+class Solution:
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        self.res = [];
+        self.backtracking([],nums);
+        return self.res;
+        
+    def backtracking(self,sub,nums):
+        if sub not in self.res:
+            self.res.append(sub[:]);
+        for i in range(len(nums)):
+            sub.append(nums[i]);
+            self.backtracking(sorted(sub),nums[:i]+nums[i+1:]);
+            sub.pop();

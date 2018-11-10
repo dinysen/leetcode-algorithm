ac0068d56f988805c41b0147895aebdb1878e392
DinY_xiaohuli <1127607894@qq.com>
2018-11-10 09:17:43 +0800

    181110

--- normal/17_电话号码的字母组合.py
+++ normal/17_电话号码的字母组合.py
@@ -28,3 +28,31 @@ class Solution:
         temp = map(lambda x : s[x],digits);
         result = reduce(lambda x,y : [a+b for a in x for b in y],temp);
         return result;
+
+#递归-------------------------------------------------
+class Solution:
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        m = {
+            "2" : "abc",
+            "3" : "def",
+            "4" : "ghi",
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv",
+            "9" : "wxyz"
+        }
+        if len(digits) == 0:
+            return [];
+        if len(digits) == 1:
+            return list(m[digits]);
+        res = [];
+        tmp = self.letterCombinations(digits[1:]);
+        for i in m[digits[0]]:
+            for j in tmp:
+                res.append(i+j);
+        return res;
--- normal/22_括号生成.py
+++ normal/22_括号生成.py
@@ -1,18 +1,20 @@
+#回溯法，其实就是穷举法
+#一般运用递归的方式实现
+#递归函数中需要传递还未完成的解与边界条件
 class Solution:
     def generateParenthesis(self, n):
         """
         :type n: int
         :rtype: List[str]
         """
-        self.res = [];
-        self.generateParentthesisIter("",n,n);
-        return self.res;
+        res = self.dfs("",[],n,n);
+        return res;
         
-    def generateParentthesisIter(self,content,l,r):
-        if l == 0 and r == 0:
-            self.res.append(content);
-        if l > 0:
-            self.generateParentthesisIter(content+"(",l-1,r);
-        if r > l:
-            self.generateParentthesisIter(content+")",l,r-1);
-            
+    def dfs(self,sub,res,lnum,rnum):
+        if lnum == 0 and rnum == 0:
+            res.append(sub);
+        if rnum > lnum:
+            self.dfs(sub+")",res,lnum,rnum-1);
+        if lnum > 0:
+            self.dfs(sub+"(",res,lnum-1,rnum);
+        return res;

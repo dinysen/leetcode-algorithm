de5c3ecf58feac3de8121ef844e5c68be4522c99
DinY_xiaohuli <1127607894@qq.com>
2018-12-02 11:17:28 +0800

    181202

--- easy/28_实现strStr().py
+++ easy/28_实现strStr().py
@@ -1,34 +1,18 @@
-#/usr/bin/env/ python3
-#-*- coding: utf-8 -*-
-
-#给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
-
-#解答：
-class Solution:
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        if len(needle) == 0:
-            return 0;
-        for i in range(len(haystack)-len(needle)+1):
-            if haystack[i:i+len(needle)] == needle:
-                return i;
-        return -1;
-
-#KMP next方法应该还能优化
 class Solution:
     
     def getNext(self,needle):
-        self.next = [-1];
-        for i in range(1,len(needle)):
-            sub = needle[:i];
-            l = len(sub)-1;
-            while l > 0 and sub[:l] != sub[len(sub)-l:len(sub)]:
-                l -= 1;
-            self.next.append(l);
+        self.next = [ 0 for i in range(len(needle)) ];
+        self.next[0] = -1;
+        n_l = list(needle);
+        k = -1;
+        j = 0;
+        while j < len(needle)-1:
+            if k == -1 or n_l[k] == n_l[j]:
+                k += 1;
+                j += 1;
+                self.next[j] = k;
+            else:
+                k = self.next[k];
                 
             
     
@@ -43,7 +27,6 @@ class Solution:
         self.getNext(needle);
         i,j = 0,0;
         while i < len(haystack):
-            print(i,j,haystack[i] == needle[j]);
             if haystack[i] == needle[j]:
                 if j == len(needle)-1:
                     return i - len(needle) + 1;

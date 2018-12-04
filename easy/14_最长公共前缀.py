b0eb418fdeaa3686f8c5ccd4aaf8df2c2d654fd2
DinY_xiaohuli <1127607894@qq.com>
2018-12-04 21:35:47 +0800

    181204

--- easy/14_最长公共前缀.py
+++ easy/14_最长公共前缀.py
@@ -1,15 +1,5 @@
-#/usr/bin/env/ python3
-#-*- coding: utf-8 -*-
-
-#从排序数组中删除重复项
-#编写一个函数来查找字符串数组中的最长公共前缀。
-
-#如果不存在公共前缀，返回空字符串 ""。
-
-#解答：
-#先找出数组中长度最短的一项，遍历此项
+from functools import reduce;
 class Solution:
-    
     def longestCommonPrefix(self, strs):
         """
         :type strs: List[str]
@@ -17,12 +7,12 @@ class Solution:
         """
         if len(strs) == 0:
             return "";
-        from functools import reduce
-        shortest_str = reduce(lambda x,y : x if len(x) < len(y) else y,strs);
-        if len(shortest_str) == 0:
-            return "";
-        for index,val in enumerate(shortest_str):
-            for j in strs:
-                if shortest_str[:index+1] != j[:index+1]:
-                    return "" if index == 0 else shortest_str[:index];
-        return shortest_str[:index+1];
+        res = [];
+        shortest_item = reduce(lambda x,y : y if len(x) > len(y) else x,strs);
+        for i in range(len(shortest_item)):
+            strs_set = set(map(lambda x : x[:i+1],strs));
+            if len(strs_set) == 1:
+                res.append(shortest_item[i]);
+            else:
+                return "".join(res);
+        return "".join(res);
--- easy/28_实现strStr().py
+++ easy/28_实现strStr().py
@@ -1,20 +1,18 @@
 class Solution:
     
     def getNext(self,needle):
-        self.next = [ 0 for i in range(len(needle)) ];
-        self.next[0] = -1;
-        n_l = list(needle);
+        res = [0 for i in range(len(needle))];
+        res[0] = -1;
         k = -1;
         j = 0;
         while j < len(needle)-1:
-            if k == -1 or n_l[k] == n_l[j]:
+            if k == -1 or needle[k] == needle[j]:
                 k += 1;
                 j += 1;
-                self.next[j] = k;
+                res[j] = k;
             else:
-                k = self.next[k];
-                
-            
+                k = res[k];
+        return res;
     
     def strStr(self, haystack, needle):
         """
@@ -22,19 +20,20 @@ class Solution:
         :type needle: str
         :rtype: int
         """
-        if len(needle) == 0:
+        if len(needle) == 0 :
             return 0;
-        self.getNext(needle);
+        next_arr = self.getNext(needle);
         i,j = 0,0;
         while i < len(haystack):
             if haystack[i] == needle[j]:
                 if j == len(needle)-1:
                     return i - len(needle) + 1;
-                i+=1;
-                j+=1;
+                i += 1;
+                j += 1;
             else:
-                j = self.next[j];
+                j = next_arr[j];
                 if j == -1:
-                    i +=1;
+                    i += 1;
                     j = 0;
         return -1;
+                
